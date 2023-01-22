ef072be5dd6b9f3b9aff08a7d5eb7339cec28dcd
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-22 12:53:41 -0800

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by youlserf on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
